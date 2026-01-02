a9e065415c8873711617935b276b997f059585e8
Nitin <rajnitin793@gmail.com>
2026-01-02 23:56:11 +0530

    dd

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ const app = express();
 const PORT = 8000;
 
 app.use(express.json());
-
+app.use(cors());
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('Server is live!'));
 app.use("/api/inngest", serve({ client: inngest, functions }));
