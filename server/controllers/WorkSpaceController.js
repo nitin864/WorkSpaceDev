dfb75910e1d6bd16572cbd5cf85258880767aa31
Nitin <rajnitin793@gmail.com>
2026-01-03 19:43:21 +0530

    added controllers for worksapce

---
+++ server/controllers/WorkSpaceController.js
@@ -0,0 +1,55 @@
+
+
+//get all WorkSpaces for user
+
+import prisma from "../configs/prisma";
+
+export const getUserWorkSpace = async (req ,res) => {
+    try {
+        const {userId} = await req.auth();
+        const workspaces = await prisma.workspace.findMany({
+            where :{
+                members: {some: {userId : userId}}
+            },
+            include: {
+                members: {include: {user: true}},
+                projects: {
+                    include: {
+                        tasks : {include: {assignee: true, comments: {include: 
+                            {user: true}}}},
+                            members: {include: {user: true}}
+                    }
+                },
+                owner: true
+            }
+        });
+        res.json({workspaces});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: error.code || error.message})
+    }
+}
+
+//Add member to Workspace
+export const addMember = async(req ,res) =>{
+    try {
+     const {userId} = await req.auth();
+     const {email,role,workspaceId, message} = req.body;
+
+     //check if user exist
+     const user = await prisma.user.findUnique({where: {email}});
+
+     if(!user){
+        return res.status(404).json({message: "User not found"})
+     }
+     
+     if(!workspaceId || role){
+        return res.status(404).json({message: "User not found"})
+     }
+
+     con
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: error.code || error.message})
+    }
+}
